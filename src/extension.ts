3dc283e02dae34102a49e1763202975a47f7fbb2
smartph <phuc.nguyen@smartpesa.com>
2022-03-01 08:03:02 +0700

    simple pre-process search string

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,11 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerTextEditorCommand("dumpvn.googleSearch", () => {
-		const selectedText = getSelectedText();
+		let selectedText = getSelectedText();
 		if (selectedText) {
+			selectedText = selectedText.replace(/^(#*)(\s*)/,'');
+			selectedText = selectedText.replace(/^(\s*\/*)(\s*)/,'');
+            
 			vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.google.com/search?q=${encodeURI(selectedText)}`))
 		}
 	});
